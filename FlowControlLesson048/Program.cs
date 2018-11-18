b6ad347dc87fe9e66a05b8e31f1c41a62158f0fe
John Lundgren <john.paul.lundgren@gmail.com>
2018-11-18 12:07:05 +0100

    Initialisation code for creating exercises.

---
+++ FlowControlLesson048/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FlowControlLesson048
+{
+    class MainClass
+    {
+
+        public static void DoExercise_1()
+        {
+            Console.WriteLine("Exercise 1 done");
+        }
+
+        public static void Main(string[] args)
+        {
+            bool quit = false;
+
+            while(!quit)
+            {
+                Console.WriteLine("1. Divisible by 3");
+                int exerciseNr = int.Parse(Console.ReadLine());
+
+                switch(exerciseNr)
+                {
+                    case 1:
+                        DoExercise_1();
+                        break;
+                    default:
+                        Console.WriteLine("Exercise {0} unavailable", exerciseNr);
+                        break;
+                }
+
+                Console.Write("Quit? (y/n): ");
+                string answer = Console.ReadLine().ToLower();
+                quit = answer == "y";
+            }
+        }
+    }
+}
