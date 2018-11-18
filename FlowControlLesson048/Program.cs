eabcf202b78fe45a790d331d590119e96f4cb9ad
John Lundgren <john.paul.lundgren@gmail.com>
2018-11-18 12:56:05 +0100

    Exercise 2 implemented.

--- FlowControlLesson048/Program.cs
+++ FlowControlLesson048/Program.cs
@@ -20,19 +20,39 @@ namespace FlowControlLesson048
             Console.WriteLine("Between 1 and 100 there are {0} divisible by 3 with no remainder", counter);
         }
 
+        public static void DoExercise_2()
+        {
+            int sum = 0;
+            while(true)
+            {
+                Console.Write("Enter a number or \'ok\' to quit: ");
+                string answer = Console.ReadLine().ToLower();
+                if(answer == "ok")
+                {
+                    Console.WriteLine("The sum of the entered values is {0}", sum);
+                    break;
+                }
+                int number = int.Parse(answer);
+                sum += number;
+            }
+        }
+
         public static void Main(string[] args)
         {
             bool quit = false;
 
             while(!quit)
             {
-                Console.WriteLine("1. Divisible by 3");
+                Console.WriteLine("1. Divisible by 3\n2. Enter a number");
                 int exerciseNr = int.Parse(Console.ReadLine());
 
                 switch(exerciseNr)
                 {
                     case 1:
                         DoExercise_1();
+                        break;
+                    case 2:
+                        DoExercise_2();
                         break;
                     default:
                         Console.WriteLine("Exercise {0} unavailable", exerciseNr);
