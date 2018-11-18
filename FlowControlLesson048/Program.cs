9c3d9ec4aa728d90e081a45d67b7691c455e950f
John Lundgren <john.paul.lundgren@gmail.com>
2018-11-18 12:26:00 +0100

    Exercise 1 implemented.

--- FlowControlLesson048/Program.cs
+++ FlowControlLesson048/Program.cs
@@ -7,7 +7,17 @@ namespace FlowControlLesson048
 
         public static void DoExercise_1()
         {
-            Console.WriteLine("Exercise 1 done");
+            int counter = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine("Between 1 and 100 there are {0} divisible by 3 with no remainder", counter);
         }
 
         public static void Main(string[] args)
